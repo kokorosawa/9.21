6006857b6d425bdf6015ef6d0c0c806fa2b6b67e
kokorosawa <90631564+kokorosawa@users.noreply.github.com>
2022-02-11 23:24:36 +0800

    Create 35.c

---
+++ Binary Search/35. Search Insert Position/35.c
@@ -0,0 +1,11 @@
+int searchInsert(int* nums, int numsSize, int target){
+    int left=0,right=numsSize,mid;
+    while(left<right){
+        mid=(left+right)/2;
+        if(nums[mid]>=target)
+            right=mid;
+        else
+            left=mid+1;
+    }
+    return left;
+}
