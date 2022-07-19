48aa2a6c6b0a81446d44cdf1cb65ab9292274c0d
kokorosawa <matt920404@gmail.com>
2022-07-19 16:44:38 +0800

    Notion

---
+++ Array/118. Pascal's Triangle/118.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> dp(numRows);
+        dp[0].push_back(1);
+        
+        for(int i = 1; i < dp.size(); i++){
+            dp[i].push_back(1);
+            for(int j = 1; j < i ; j++){
+                int temp = dp[i - 1][j - 1] + dp[i - 1][j];
+                dp[i].push_back(temp);
+            }
+            dp[i].push_back(1);
+        }
+        return dp;
+    }
+};
